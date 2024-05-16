f8d8cd573aa22c5d44b517de4aeea941648b042b
Rahman Mahmoodi <rahman.mahmoodi@gmail.com>
2024-05-16 12:30:19 +1000

    rahman test

--- My_API/Controllers/ValuesDao.cs
+++ My_API/Controllers/ValuesDao.cs
@@ -29,6 +29,33 @@ namespace My_API.Controllers {
                 }
             }
             return null;
+        } 
+        
+        // TODO: Just for test,this is just for testing if two VIs are created for this
+        public string getValue2(string id) {
+                        var connection = new SqlConnection();
+            try
+            {
+                connection.ConnectionString = "db info";
+                connection.Open();
+                var selectSql = string.Format("select from MyStuff where id='{0}';", id);
+                var selectCommand = new SqlCommand(selectSql, connection);
+
+                var dataReader = selectCommand.ExecuteReader();
+                return dataReader.GetString(0);
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                if (connection.State == ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+            }
+
+            return null;
         }
     }
 }
